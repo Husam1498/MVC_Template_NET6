8c26e0945edb4aef3b32dc61a60936775ca50384
Hüsamettin Demir <65946907+Husam1498@users.noreply.github.com>
2024-07-04 13:49:05 +0300

    Login giriş sayfası ve validation işlemleri yapıldı

---
+++ MVC_Template_NET6/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Template_NET6.Models;
+
+namespace MVC_Template_NET6.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //Login işlemleri
+            }
+            return View(model);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Template_NET6/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Template_NET6.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı adı zorunlu")]
+        [StringLength(10, ErrorMessage = "En fazla 10 karakter girimeli")]
+        public string Username { get; set; }
+
+        
+        [Required(ErrorMessage = "Şifre girmek zorunlu adı zorunlu")]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakter girilmeli")]
+        [MaxLength(10, ErrorMessage = "Şifreniz en fazla 10 karakter girilmeli")]
+        public string Password { get; set; }
+        
+
+
+    }
+}
